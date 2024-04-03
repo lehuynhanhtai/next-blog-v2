c664ed1f8a6978dd4c1bdb278189a86dc31bb600
lehuynhanhtai <lehuynhanhtai@gmail.com>
2024-04-04 06:36:37 +0700

    setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <h1>tai</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>tai</h1>
+        </div>
+    );
 }
